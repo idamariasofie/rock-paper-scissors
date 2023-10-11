16360357e83c40473a38152d0991b91396c89d6c
idamariasofie <>
2023-10-11 10:27:33 +0000

    Added basic JavaScript structure and eventlisteners for click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log("Connected!")
+// Wait for DOM to finish lodaing before running the game
+document.addEventListener("DOMContentLoaded", function() {
+let buttons = document.getElementsByTagName("button");
+
+for (let button of buttons){
+    button.addEventListener("click", function() {
+
+    })
+
+}
+
+})
+
+// Get the button elements and add event listeners to them
+
+
+function runGame() {
+
+}
+
+
+function selectButton() {
+
+}
+
